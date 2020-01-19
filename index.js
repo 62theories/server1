78b7d317b6ecd6d3cfc85d8881c2010142f52dba
ball <nattawatx86@gmail.com>
2020-01-20 00:39:21 +0700

    no idea what i am doing

--- index.js
+++ index.js
@@ -14,15 +14,35 @@
 // app.listen(PORT, () => {
 //       console.log("server on port:", PORT)
 // })
-const WebSocket = require("ws")
-const wss = new WebSocket.Server({ port: 3000 })
 
-wss.on("connection", function connection(ws) {
-      ws.on("message", function incoming(message) {
-            console.log("received: %s", message)
-      })
+// const WebSocket = require("ws")
+// const wss = new WebSocket.Server({ port: 3000 })
+
+// wss.on("connection", function connection(ws) {
+//       ws.on("message", function incoming(message) {
+//             console.log("received: %s", message)
+//       })
+// })
+
+// wss.on("close", function close() {
+//       console.log("disconnected")
+// })
+
+const dgram = require("dgram")
+const server = dgram.createSocket("udp4")
+
+server.on("error", err => {
+      console.log(`server error:\n${err.stack}`)
+      server.close()
 })
 
-wss.on("close", function close() {
-      console.log("disconnected")
+server.on("message", (msg, rinfo) => {
+      console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`)
 })
+
+server.on("listening", () => {
+      const address = server.address()
+      console.log(`server listening ${address.address}:${address.port}`)
+})
+
+server.bind(80)
