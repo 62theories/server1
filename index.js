5928279fc39b74f6ab6c852fc72a67d1b2e0491e
ball <nattawatx86@gmail.com>
2020-01-20 01:01:22 +0700

    wssssss

--- index.js
+++ index.js
@@ -15,34 +15,34 @@
 //       console.log("server on port:", PORT)
 // })
 
-// const WebSocket = require("ws")
-// const wss = new WebSocket.Server({ port: 3000 })
+const WebSocket = require("ws")
+const wss = new WebSocket.Server({ port: 3000 })
 
-// wss.on("connection", function connection(ws) {
-//       ws.on("message", function incoming(message) {
-//             console.log("received: %s", message)
-//       })
+wss.on("connection", function connection(ws) {
+      ws.on("message", function incoming(message) {
+            console.log("received: %s", message)
+      })
+})
+
+wss.on("close", function close() {
+      console.log("disconnected")
+})
+
+// const dgram = require("dgram")
+// const server = dgram.createSocket("udp4")
+
+// server.on("error", err => {
+//       console.log(`server error:\n${err.stack}`)
+//       server.close()
 // })
 
-// wss.on("close", function close() {
-//       console.log("disconnected")
+// server.on("message", (msg, rinfo) => {
+//       console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`)
 // })
 
-const dgram = require("dgram")
-const server = dgram.createSocket("udp4")
+// server.on("listening", () => {
+//       const address = server.address()
+//       console.log(`server listening ${address.address}:${address.port}`)
+// })
 
-server.on("error", err => {
-      console.log(`server error:\n${err.stack}`)
-      server.close()
-})
-
-server.on("message", (msg, rinfo) => {
-      console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`)
-})
-
-server.on("listening", () => {
-      const address = server.address()
-      console.log(`server listening ${address.address}:${address.port}`)
-})
-
-server.bind(80)
+// server.bind(80)
