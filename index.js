6d1a5868824ddb9154c8fe06e6ef27d8fa05b11d
ball <nattawatx86@gmail.com>
2020-01-20 00:07:31 +0700

    ws

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
+// const express = require("express")
+// const cors = require("cors")
+// const app = express()
 
-app.use(cors())
+// app.use(cors())
 
-app.get("/", (req, res) => {
-      res.send({
-            hello: "there"
+// app.get("/", (req, res) => {
+//       res.send({
+//             hello: "there"
+//       })
+// })
+
+// const PORT = process.env.PORT || 3000
+// app.listen(PORT, () => {
+//       console.log("server on port:", PORT)
+// })
+const WebSocket = require("ws")
+const wss = new WebSocket.Server({ port: 3000 })
+
+wss.on("connection", function connection(ws) {
+      ws.on("message", function incoming(message) {
+            console.log("received: %s", message)
       })
 })
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-      console.log("server on port:", PORT)
+wss.on("close", function close() {
+      console.log("disconnected")
 })
