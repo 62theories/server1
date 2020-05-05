b0d132cc86063246c8bf0f3ede6f9eb8314a5af8
ball <nattawatx86@gmail.com>
2020-05-06 00:59:42 +0700

    log

--- index.js
+++ index.js
@@ -11,155 +11,155 @@ server.on("error", (err) => {
 
 server.on("message", (msg, rinfo) => {
       console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`)
-      let time = new Date()
-      let convert = ""
-      try {
-            convert = JSON.parse(msg)
-      } catch (err) {
-            console.log(err)
-      } finally {
-            if (convert.DEAUTH || convert.DEAUTH === 0) {
-                  axios.post(
-                        "https://finalprojectcoe.firebaseio.com/deauth.json",
-                        {
-                              count: +convert.DEAUTH,
-                              time: time,
-                        },
-                        { headers: { "Content-Type": "application/json" } }
-                  ).then(() =>
-                        console.log(
-                              "SENT to https://finalprojectcoe.firebaseio.com/deauth.json"
-                        )
-                  )
-                  axios.get(
-                        "https://finalprojectcoe.firebaseio.com/alert/deauth.json"
-                  ).then((alertFetch) => {
-                        const { data } = alertFetch
-                        if (data && +convert.DEAUTH > data) {
-                              axios.request({
-                                    method: "POST",
-                                    url:
-                                          "https://notify-api.line.me/api/notify",
-                                    data: qs.stringify({
-                                          message: `DEAUTH ATTACK FOUND (${+convert.DEAUTH} packets)`,
-                                    }),
-                                    headers: {
-                                          "Content-Type":
-                                                "application/x-www-form-urlencoded",
-                                          Authorization: `Bearer S5V1TfvbIQxTCaSLF0KyIJPNS8FndGyi6goUwrI6Evs`,
-                                    },
-                              })
-                              axios.post(
-                                    "https://finalprojectcoe.firebaseio.com/notification/deauth.json",
-                                    {
-                                          count: +convert.DEAUTH,
-                                          time: time,
-                                    },
-                                    {
-                                          headers: {
-                                                "Content-Type":
-                                                      "application/json",
-                                          },
-                                    }
-                              )
-                        }
-                  })
-            }
-            if (convert.PROBE || convert.PROBE === 0) {
-                  axios.post(
-                        "https://finalprojectcoe.firebaseio.com/probe.json",
-                        {
-                              count: +convert.PROBE,
-                              time: time,
-                        },
-                        { headers: { "Content-Type": "application/json" } }
-                  ).then(() =>
-                        console.log(
-                              "SENT to https://finalprojectcoe.firebaseio.com/probe.json"
-                        )
-                  )
-                  axios.get(
-                        "https://finalprojectcoe.firebaseio.com/alert/probe.json"
-                  ).then((alertFetch) => {
-                        const { data } = alertFetch
-                        if (data && +convert.PROBE > data) {
-                              axios.request({
-                                    method: "POST",
-                                    url:
-                                          "https://notify-api.line.me/api/notify",
-                                    data: qs.stringify({
-                                          message: `PROBE ATTACK FOUND (${+convert.PROBE} packets)`,
-                                    }),
-                                    headers: {
-                                          "Content-Type":
-                                                "application/x-www-form-urlencoded",
-                                          Authorization: `Bearer S5V1TfvbIQxTCaSLF0KyIJPNS8FndGyi6goUwrI6Evs`,
-                                    },
-                              })
-                              axios.post(
-                                    "https://finalprojectcoe.firebaseio.com/notification/probe.json",
-                                    {
-                                          count: +convert.PROBE,
-                                          time: time,
-                                    },
-                                    {
-                                          headers: {
-                                                "Content-Type":
-                                                      "application/json",
-                                          },
-                                    }
-                              )
-                        }
-                  })
-            }
-            if (convert.BEACON || convert.BEACON === 0) {
-                  axios.post(
-                        "https://finalprojectcoe.firebaseio.com/beacon.json",
-                        {
-                              count: +convert.BEACON,
-                              time: time,
-                        },
-                        { headers: { "Content-Type": "application/json" } }
-                  ).then(() =>
-                        console.log(
-                              "SENT to https://finalprojectcoe.firebaseio.com/beacon.json"
-                        )
-                  )
-                  axios.get(
-                        "https://finalprojectcoe.firebaseio.com/alert/beacon.json"
-                  ).then((alertFetch) => {
-                        const { data } = alertFetch
-                        if (data && +convert.BEACON > data) {
-                              axios.request({
-                                    method: "POST",
-                                    url:
-                                          "https://notify-api.line.me/api/notify",
-                                    data: qs.stringify({
-                                          message: `beacon ATTACK FOUND (${+convert.BEACON} packets)`,
-                                    }),
-                                    headers: {
-                                          "Content-Type":
-                                                "application/x-www-form-urlencoded",
-                                          Authorization: `Bearer S5V1TfvbIQxTCaSLF0KyIJPNS8FndGyi6goUwrI6Evs`,
-                                    },
-                              })
-                              axios.post(
-                                    "https://finalprojectcoe.firebaseio.com/notification/beacon.json",
-                                    {
-                                          count: +convert.BEACON,
-                                          time: time,
-                                    },
-                                    {
-                                          headers: {
-                                                "Content-Type":
-                                                      "application/json",
-                                          },
-                                    }
-                              )
-                        }
-                  })
-            }
-      }
+      // let time = new Date()
+      // let convert = ""
+      // try {
+      //       convert = JSON.parse(msg)
+      // } catch (err) {
+      //       console.log(err)
+      // } finally {
+      //       if (convert.DEAUTH || convert.DEAUTH === 0) {
+      //             axios.post(
+      //                   "https://finalprojectcoe.firebaseio.com/deauth.json",
+      //                   {
+      //                         count: +convert.DEAUTH,
+      //                         time: time,
+      //                   },
+      //                   { headers: { "Content-Type": "application/json" } }
+      //             ).then(() =>
+      //                   console.log(
+      //                         "SENT to https://finalprojectcoe.firebaseio.com/deauth.json"
+      //                   )
+      //             )
+      //             axios.get(
+      //                   "https://finalprojectcoe.firebaseio.com/alert/deauth.json"
+      //             ).then((alertFetch) => {
+      //                   const { data } = alertFetch
+      //                   if (data && +convert.DEAUTH > data) {
+      //                         axios.request({
+      //                               method: "POST",
+      //                               url:
+      //                                     "https://notify-api.line.me/api/notify",
+      //                               data: qs.stringify({
+      //                                     message: `DEAUTH ATTACK FOUND (${+convert.DEAUTH} packets)`,
+      //                               }),
+      //                               headers: {
+      //                                     "Content-Type":
+      //                                           "application/x-www-form-urlencoded",
+      //                                     Authorization: `Bearer S5V1TfvbIQxTCaSLF0KyIJPNS8FndGyi6goUwrI6Evs`,
+      //                               },
+      //                         })
+      //                         axios.post(
+      //                               "https://finalprojectcoe.firebaseio.com/notification/deauth.json",
+      //                               {
+      //                                     count: +convert.DEAUTH,
+      //                                     time: time,
+      //                               },
+      //                               {
+      //                                     headers: {
+      //                                           "Content-Type":
+      //                                                 "application/json",
+      //                                     },
+      //                               }
+      //                         )
+      //                   }
+      //             })
+      //       }
+      //       if (convert.PROBE || convert.PROBE === 0) {
+      //             axios.post(
+      //                   "https://finalprojectcoe.firebaseio.com/probe.json",
+      //                   {
+      //                         count: +convert.PROBE,
+      //                         time: time,
+      //                   },
+      //                   { headers: { "Content-Type": "application/json" } }
+      //             ).then(() =>
+      //                   console.log(
+      //                         "SENT to https://finalprojectcoe.firebaseio.com/probe.json"
+      //                   )
+      //             )
+      //             axios.get(
+      //                   "https://finalprojectcoe.firebaseio.com/alert/probe.json"
+      //             ).then((alertFetch) => {
+      //                   const { data } = alertFetch
+      //                   if (data && +convert.PROBE > data) {
+      //                         axios.request({
+      //                               method: "POST",
+      //                               url:
+      //                                     "https://notify-api.line.me/api/notify",
+      //                               data: qs.stringify({
+      //                                     message: `PROBE ATTACK FOUND (${+convert.PROBE} packets)`,
+      //                               }),
+      //                               headers: {
+      //                                     "Content-Type":
+      //                                           "application/x-www-form-urlencoded",
+      //                                     Authorization: `Bearer S5V1TfvbIQxTCaSLF0KyIJPNS8FndGyi6goUwrI6Evs`,
+      //                               },
+      //                         })
+      //                         axios.post(
+      //                               "https://finalprojectcoe.firebaseio.com/notification/probe.json",
+      //                               {
+      //                                     count: +convert.PROBE,
+      //                                     time: time,
+      //                               },
+      //                               {
+      //                                     headers: {
+      //                                           "Content-Type":
+      //                                                 "application/json",
+      //                                     },
+      //                               }
+      //                         )
+      //                   }
+      //             })
+      //       }
+      //       if (convert.BEACON || convert.BEACON === 0) {
+      //             axios.post(
+      //                   "https://finalprojectcoe.firebaseio.com/beacon.json",
+      //                   {
+      //                         count: +convert.BEACON,
+      //                         time: time,
+      //                   },
+      //                   { headers: { "Content-Type": "application/json" } }
+      //             ).then(() =>
+      //                   console.log(
+      //                         "SENT to https://finalprojectcoe.firebaseio.com/beacon.json"
+      //                   )
+      //             )
+      //             axios.get(
+      //                   "https://finalprojectcoe.firebaseio.com/alert/beacon.json"
+      //             ).then((alertFetch) => {
+      //                   const { data } = alertFetch
+      //                   if (data && +convert.BEACON > data) {
+      //                         axios.request({
+      //                               method: "POST",
+      //                               url:
+      //                                     "https://notify-api.line.me/api/notify",
+      //                               data: qs.stringify({
+      //                                     message: `beacon ATTACK FOUND (${+convert.BEACON} packets)`,
+      //                               }),
+      //                               headers: {
+      //                                     "Content-Type":
+      //                                           "application/x-www-form-urlencoded",
+      //                                     Authorization: `Bearer S5V1TfvbIQxTCaSLF0KyIJPNS8FndGyi6goUwrI6Evs`,
+      //                               },
+      //                         })
+      //                         axios.post(
+      //                               "https://finalprojectcoe.firebaseio.com/notification/beacon.json",
+      //                               {
+      //                                     count: +convert.BEACON,
+      //                                     time: time,
+      //                               },
+      //                               {
+      //                                     headers: {
+      //                                           "Content-Type":
+      //                                                 "application/json",
+      //                                     },
+      //                               }
+      //                         )
+      //                   }
+      //             })
+      //       }
+      // }
 })
 
 server.on("listening", () => {
